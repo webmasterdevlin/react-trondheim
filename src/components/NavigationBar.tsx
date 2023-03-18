c0cfff9ed3d280527b43ca5ff070b6e262dae728
devlinduldulao <webmasterdevlin@gmail.com>
2023-03-18 11:58:32 +0100

    update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 
 export default function NavigationBar() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
-    <nav>
-      <div>
-        <div>
-          <div>
+    <nav className="w-full bg-gray-100 pb-10 font-sans shadow">
+      <div className="bg-blue-900">
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-start gap-2 py-4">
             <button
+              className={`mr-2 mb-2 rounded-lg px-5 py-2.5 text-center text-sm font-medium uppercase
+              ${location.pathname === '/' ? 'bg-blue-200' : 'bg-gray-100'}
+              `}
               onClick={() => {
                 navigate('/');
               }}
