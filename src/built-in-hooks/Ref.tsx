3f49e3bd7d004a597ace3da6acadb6f935fdd3c3
devlinduldulao <webmasterdevlin@gmail.com>
2023-03-19 13:45:34 +0100

    update

--- src/built-in-hooks/Ref.tsx
+++ src/built-in-hooks/Ref.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
+import VideoPlayer from 'react-video-player';
 import Button from '../components/Button';
+import Card from './Card';
+import type { VideoPlayerType } from 'react-video-player';
 // the useRef hook allows to persist values between renders
 // it can be used to store a mutable value that does not cause a re-render when updated
 // it can be used to access a DOM element directly
 export default function Ref() {
-  const [name, setName] = useState('');
+  const [name] = useState('');
   const prevNameRef = useRef('');
   const inputRef = useRef<HTMLInputElement>(null);
   const focus = () => {
@@ -15,16 +18,39 @@ export default function Ref() {
   }, [inputRef.current?.value as string]);
   console.log('rendered');
 
+  const videoPlayer = useRef<VideoPlayerType>();
+
+  const handleVideoPlay = () => {
+    videoPlayer.current.thumbUrl = 'http://.webp';
+    videoPlayer.current.play();
+  };
+  const handleVideoPause = () => {
+    videoPlayer.current.thumbUrl = 'http://.png';
+    videoPlayer.current.pause();
+  };
+  const handleVideoStop = () => {
+    videoPlayer.current.thumbUrl = 'http://.jpg';
+    videoPlayer.current.stop();
+  };
   return (
     <>
+      <Thumbnail url={videoPlayer.current.thumbUrl} />
+      <VideoPlayer path={'http://.mp4'} ref={videoPlayer} />
+      <button onClick={handleVideoPlay}>Play</button>
+      <button onClick={handleVideoPause}>Pause</button>
+      <button onClick={handleVideoStop}>Stop</button>
+
       <input
-        value={inputRef.current?.value}
+        ref={inputRef} // inputRef.current == input element
         onChange={e => {
+          if (inputRef.current) {
+            inputRef.current.value = e.target.value;
+          }
           console.log('logs:', {
             event: e.target.value,
             inputRef: inputRef.current?.value,
           });
-          setName(e.target.value); // causes re-render
+          // setName(e.target.value); // causes re-render
         }}
       />
       <div>
@@ -34,6 +60,7 @@ export default function Ref() {
       <Button color="primary" onClick={focus}>
         Focus
       </Button>
+      <Card name={inputRef?.current?.value as string} />
     </>
   );
 }
